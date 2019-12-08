b4f6315fe32ffc89a0bd00854cb0ffa4577e79a8
VIGNESHWAR258 <36541704+VIGNESHWAR258@users.noreply.github.com>
2019-12-08 14:35:04 +0530

    Add files via upload

---
+++ Assignments/Python-Data-Structures/assignment9_4.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 22:05:07 2019
+
+@author: VIGNESHWAR REDDY
+"""
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+lst = list()
+dic = dict()
+handle = open(name)
+for line in handle:
+    if line.startswith('From'):
+        if line.startswith('From:'):
+            continue
+        lst = line.split()
+        dic[lst[1]] = dic.get(lst[1],0) + 1 
+mostrepeated = None
+nooftimes = None       
+for key,value in dic.items():
+    if nooftimes is None or value > nooftimes:
+        mostrepeated = key
+        nooftimes = value
+print(mostrepeated,nooftimes)
